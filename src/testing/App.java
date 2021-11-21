a4adeaa4b11175856e709b47ce7fa5cbaa94d0eb
deformal <drake9211@gmail.com>
2021-11-22 00:34:50 +0530

    initial

---
+++ src/testing/App.java
@@ -0,0 +1,9 @@
+package testing;
+public class App {
+    public static void main(String args []) {
+        Helpers x = new Helpers();
+        x.SayHello(); 
+        x.Sum();
+        System.out.println("Hello World");
+    }
+}
---
+++ src/testing/Helpers.java
@@ -0,0 +1,20 @@
+package testing;
+import java.util.*;
+public class Helpers {
+
+    public void SayHello(){
+        System.out.println("Sup");
+    }
+
+    public void Sum(){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Please enter first number");
+     Integer firstNum = sc.nextInt();
+     System.out.println("Please enter second number");
+     Integer secondNum = sc.nextInt();
+     Integer sum = firstNum + secondNum;
+     System.out.println(sum);
+     sc.close();
+    }
+}
+
