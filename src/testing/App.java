9fc340980629e3f1a7f08c9eb81146ba5b7de148
deformal <saurabh@raftlabs.co>
2021-11-25 04:17:25 +0530

    Added simple.json external library, also finished the getStarWarsPeople function.

--- src/testing/App.java
+++ src/testing/App.java
@@ -4,7 +4,7 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 public class App {
     public static void main(String... args) throws IOException, InterruptedException, ParseException {
-        Httpmodule newRequest = new Httpmodule("2");
+        Httpmodule newRequest = new Httpmodule("1"); // fetches data for luke skywalker
         newRequest.getStarWarsPeople();
     }
 }
