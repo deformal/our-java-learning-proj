3eab8c2b0f2e6c316612428bf1ba95a2273f4a9a
deformal <saurabh@raftlabs.co>
2021-11-25 03:51:50 +0530

    Added simple.json external library, also finished the getStarWarsPeople function.

--- src/testing/App.java
+++ src/testing/App.java
@@ -1,9 +1,10 @@
 package testing;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 public class App {
-    public static void main(String args []) {
-        Helpers x = new Helpers();
-        x.SayHello(); 
-        x.Sum();
-        System.out.println("Hello World");
+    public static void main(String... args) throws IOException, InterruptedException, ParseException {
+        Httpmodule newRequest = new Httpmodule("2");
+        newRequest.getStarWarsPeople();
     }
 }
--- src/testing/Helpers.java
+++ src/testing/Helpers.java
@@ -1,23 +1,25 @@
 package testing;
+
 import java.util.*;
+
 public class Helpers {
 
-    public void SayHello(){
+    protected void SayHello() {
         System.out.println("Sup");
     }
 
-    public void Sum(){
-     Scanner sc = new Scanner(System.in);
-     System.out.println("Please enter first number");
-     Integer firstNum = sc.nextInt();
-     System.out.println("Please enter second number");
-     Integer secondNum = sc.nextInt();
-     Integer sum = firstNum + secondNum;
-     System.out.println(sum);
-     sc.close();
+    protected void Sum() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        Integer firstNum = sc.nextInt();
+        System.out.println("Please enter second number");
+        Integer secondNum = sc.nextInt();
+        Integer sum = firstNum + secondNum;
+        System.out.println(sum);
+        sc.close();
     }
-    
-    public void Sub(){
+
+    protected void Sub() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first number");
         Integer firstNum = sc.nextInt();
@@ -26,6 +28,5 @@ public class Helpers {
         Integer sub = firstNum - secondNum;
         System.out.println(sub);
         sc.close();
-       }
+    }
 }
-
