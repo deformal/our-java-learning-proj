ed543452857af7e9b7b0d855db977efd3787e0d5
deformal <saurabh@raftlabs.co>
2021-11-25 12:12:20 +0530

    No all the starwars returned data will be printed in a proper manner

--- src/testing/App.java
+++ src/testing/App.java
@@ -1,9 +1,12 @@
 package testing;
+
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+
 public class App {
-    public static void main(String... args) throws IOException, InterruptedException, ParseException {
+    public static void main(String... args)
+            throws IOException, InterruptedException, ParseException {
         Httpmodule newRequest = new Httpmodule("1"); // fetches data for luke skywalker
         newRequest.getStarWarsPeople();
     }
--- src/testing/Httpmodule.java
+++ src/testing/Httpmodule.java
@@ -1,28 +1,36 @@
 package testing;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
 
 public class Httpmodule {
-    private final URI people_url;
-    private final String new_url_str;
+    private final URI peopleUrl;
+
     protected Httpmodule(String value) {
-        this.new_url_str = "https://swapi.dev/api/people/" + value + "/?format=json";
-        this.people_url = URI.create(new_url_str);
+        String new_url_str = "https://swapi.dev/api/people/" + value + "/?format=json";
+        this.peopleUrl = URI.create(new_url_str);
     }
+
     protected void getStarWarsPeople() throws IOException, InterruptedException, ParseException {
         JSONParser jsp = new JSONParser();
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().GET().header("accept", "application/json").uri(people_url)
-                .build();
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .GET()
+                        .header("accept", "application/json")
+                        .uri(peopleUrl)
+                        .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         JSONObject result = (JSONObject) jsp.parse(response.body());
-        Object name = result.get("name");
-        System.out.println(name);
+        for (Object element : result.entrySet()) {
+            System.out.println(element);
+        }
     }
 }
